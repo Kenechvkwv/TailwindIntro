a42d0bdfd8ae56d620ff9b6304a6a9f9e1e4e5dc
kenechvkwv <ubesiekene@gmail.com>
2021-11-05 18:36:06 +0100

    index.js

---
+++ public/index.js
@@ -0,0 +1,18 @@
+// grab the DOM
+const menuButton = document.querySelector("#menu-button");
+const closeButton = document.querySelector("#x-button");
+const sidebar = document.querySelector("#sidebar");
+const body = document.querySelector("body");
+
+// add event listener
+menuButton.addEventListener("click", () => {
+  sidebar.classList.remove("-translate-x-full");
+  body.classList.add("overflow-hidden");
+  sidebar.classList.add("overlay-effect");
+});
+
+closeButton.addEventListener("click", () => {
+  sidebar.classList.add("-translate-x-full");
+  body.classList.remove("overflow-hidden");
+  sidebar.classList.remove("overlay-effect");
+});
